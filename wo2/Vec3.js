d88f3a4beb05e0c14dd6dc398f821375a64d0959
古田　琢人 <1445079t@istc-a-063.epc.center.kobe-u.ac.jp>
2017-04-12 16:23:28 +0900

    task2

--- wo2/Vec3.js
+++ wo2/Vec3.js
@@ -51,3 +51,48 @@ Vec3.prototype.mid=function()
     mid=sum-this.max()-this.min();
     return mid;
 }
+
+
+
+Vec3.prototype.minus = function(v)
+
+{
+
+this.x=v.x-this.x;
+
+this.y=v.y-this.y;
+
+this.z=v.z-this.z;
+
+return this;
+
+}
+
+Vec3.prototype.CrossProduct = function(v)
+{
+    this.x=v.y*this.z-this.y*v.z;
+    this.y=v.z*this.x-this.z*v.x;
+    this.z=v.x*this.y-this.x*v.y;
+    return this;
+}
+
+Vec3.prototype.Length=function();
+{
+    var length=0;
+    length=Math.pow(this.x,2)+Math.pow(this.y,2)+Math.pow(this.z,2);
+    length=Math.pow(length,1/2);
+    return length;
+}
+
+Vec3.prototype.AreaTriangle()=function(v1,v2)
+{
+    var length=0;
+    var CP=0;
+    var Area=0;
+    v1=v1.minus(this);
+    v2=v2.minus(this);
+    CP=v1.CrossProduct(v2);
+    length=CP.Length();
+    Area=length/2;
+    return Area;
+}
