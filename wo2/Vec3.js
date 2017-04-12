d2aff3c3f698d9070b6f1c22e3c8dc29e0d39f4e
古田　琢人 <1445079t@istc-a-063.epc.center.kobe-u.ac.jp>
2017-04-12 15:21:56 +0900

    task1

--- wo2/Vec3.js
+++ wo2/Vec3.js
@@ -1,61 +1,53 @@
-<html>
-  <head>
-    <title>wo2:Task 01</title>
-  </head>
-  <body>
-    <script>
-      // Constructor
-      Vec3 = function( x, y, z )
-      {
-      this.x = x;
-      this.y = y;
-      this.z = z;
-      }
-      
-      Vec3.prototype.max=function()
-      {
-      var max=0;
-      if(this.x>this.y)
-      {
-      max=this.x;
-      }
-      else
-      {
-      max=this.y;
-      }
-      if(this.z>max)
-      {
-      max=this.z
-      }
-      return max;
-      }
+// Constructor
+Vec3 = function( x, y, z )
+{
+    this.x = x;
+    this.y = y;
+    this.z = z;
+}
 
-      Vec3.prototype.min=function()
-      {
-      var min=0;
-      if(this.x>this.y)
-      {
-      min=this.y;
-      }
-      else
-      {
-      min=this.x;
-      }
-      if(min>this.z)
-      {
-      min=this.z
-      }
-      return min;
-      }
+Vec3.prototype.max=function()
+{
+    var max=0;
+    
+    if(this.x>this.y)
+    {
+	max=this.x;
+    }
+    else
+    {
+	max=this.y;
+    }
+    if(this.z>max)
+    {
+	max=this.z
+    }
+    return max;
+}
 
-      Vec3.prototype.mid=function()
-      {
-      var mid=0;
-      var sum=0;
-      sum=this.x+this.y+this.z;
-      mid=sum-this.max()-this.min();
-      return mid;
-      }
-    </script>
-  </body>
-</html>
+Vec3.prototype.min=function()
+{
+    var min=0;
+    if(this.x>this.y)
+    {
+	min=this.y;
+    }
+    else
+    {
+	min=this.x;
+    }
+    if(min>this.z)
+    {
+	min=this.z
+    }
+    return min;
+}
+
+Vec3.prototype.mid=function()
+{
+    var mid=0;
+    var sum=0;
+    sum=this.x+this.y+this.z;
+    mid=sum-this.max()-this.min();
+    return mid;
+}
