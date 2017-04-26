57e8c1f7a68126f10929ee38a14c31c41112d25a
古田琢人 <furutatakuto@ip252-245.wlan.kobe-u.ac.jp>
2017-04-26 14:34:58 +0900

    first

--- wo6/w06_ex02.js
+++ wo6/w06_ex02.js
@@ -102,7 +102,7 @@ function main(){
 	var x_NDC = 2*(x_win-vx)/vw-1;
 	var y_NDC = -(2*(y_win-vy)/vh-1);
 	var p_NDC = new THREE.Vector3(x_NDC,y_NDC,1);
-	var p_wld = p_NDC.unproject(camera);
+	var p_wld = p_NDC.unproject(camera2);
 	
 	var origin = camera.position;
 	var direction = p_wld.sub( camera.position ).normalize();
