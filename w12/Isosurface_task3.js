e326c49b2493350776068248ab3e2b1610c382cd
古田琢人 <furutatakuto@FURUTA-MAC.local>
2017-05-26 12:33:52 +0900

    0526-1

--- w12/Isosurface_task3.js
+++ w12/Isosurface_task3.js
@@ -1,14 +1,27 @@
-function Isosurfaces( volume, isovalue )
+function Isosurfaces( volume, isovalue ,screen)
 {
+    
+  
     var geometry = new THREE.Geometry();
-    var material = new THREE.MeshLambertMaterial();
-
+  
     var smin = volume.min_value;
     var smax = volume.max_value;
-
-    //Threshold
     isovalue = KVS.Clamp( isovalue, smin, smax );
-
+    
+    // Create color map
+    var cmap = [];
+    for ( var i = 0; i < 256 ; i++ )
+    {	
+        var S = i/255.0; // [0,1]
+        var R = Math.max( Math.cos( ( S - 1.0 ) * Math.PI ), 0.0 );
+        var G = Math.max( Math.cos( ( S - 0.5 ) * Math.PI ), 0.0 );
+        var B = Math.max( Math.cos( S * Math.PI ), 0.0 );
+        var color = new THREE.Color( R, G, B );
+        cmap.push( [ S, '0x' + color.getHexString() ] );
+    }
+    
+    var materialColor =new THREE.Color().setHex( cmap[isovalue][1]);
+    
     var material = new THREE.ShaderMaterial({
 	vertexColors: THREE.VertexColors,
 	vertexShader: document.getElementById('lambertian.vert').text,
@@ -27,32 +40,31 @@ function Isosurfaces( volume, isovalue )
         {
             for ( var x = 0; x < volume.resolution.x - 1; x++ )
             {
-		
                 var indices = cell_node_indices( cell_index++ );
                 var index = table_index( indices );
                 if ( index == 0 ) { continue; }
                 if ( index == 255 ) { continue; }
-
+		
                 for ( var j = 0; lut.edgeID[index][j] != -1; j += 3 )
                 {
                     var eid0 = lut.edgeID[index][j];
                     var eid1 = lut.edgeID[index][j+2];
                     var eid2 = lut.edgeID[index][j+1];
-
+		    
                     var vid0 = lut.vertexID[eid0][0];
                     var vid1 = lut.vertexID[eid0][1];
                     var vid2 = lut.vertexID[eid1][0];
                     var vid3 = lut.vertexID[eid1][1];
                     var vid4 = lut.vertexID[eid2][0];
                     var vid5 = lut.vertexID[eid2][1];
-
+		    
                     var v0 = new THREE.Vector3( x + vid0[0], y + vid0[1], z + vid0[2] );
                     var v1 = new THREE.Vector3( x + vid1[0], y + vid1[1], z + vid1[2] );
                     var v2 = new THREE.Vector3( x + vid2[0], y + vid2[1], z + vid2[2] );
                     var v3 = new THREE.Vector3( x + vid3[0], y + vid3[1], z + vid3[2] );
                     var v4 = new THREE.Vector3( x + vid4[0], y + vid4[1], z + vid4[2] );
                     var v5 = new THREE.Vector3( x + vid5[0], y + vid5[1], z + vid5[2] );
-
+		    
                     var v01 = interpolated_vertex( v0, v1, isovalue );
                     var v23 = interpolated_vertex( v2, v3, isovalue );
                     var v45 = interpolated_vertex( v4, v5, isovalue );
@@ -60,7 +72,7 @@ function Isosurfaces( volume, isovalue )
                     geometry.vertices.push( v01 );
                     geometry.vertices.push( v23 );
                     geometry.vertices.push( v45 );
-
+		    
                     var id0 = counter++;
                     var id1 = counter++;
                     var id2 = counter++;
@@ -71,34 +83,13 @@ function Isosurfaces( volume, isovalue )
         }
         cell_index += volume.resolution.x;
     }
-
+    
     geometry.computeVertexNormals();
-
-    //task1
-    //<<<<<<<<<< add >>>>>>>>>>>>>
-    //Create color map
-    var cmap = [];
-    var RESOLUTION = 256;
     
-    for ( var i = 0; i < RESOLUTION ; i++ )
-    {	
-        var S = i/(RESOLUTION-1); // [0,1]
-        var R = Math.max( Math.cos( ( S - 1.0 ) * Math.PI ), 0.0 );
-        var G = Math.max( Math.cos( ( S - 0.5 ) * Math.PI ), 0.0 );
-        var B = Math.max( Math.cos( S * Math.PI ), 0.0 );
-        var color = new THREE.Color( R, G, B );
-
-        cmap.push( [ S, '0x' + color.getHexString() ] );
-    }
-    
-    
-    material.color = new THREE.Color().setHex(cmap[isovalue][1]);
-     //<<<<<<<<<< /add >>>>>>>>>>>>>
-
-    
+    material.color = materialColor;
     return new THREE.Mesh( geometry, material );
-
-
+    
+    
     function cell_node_indices( cell_index )
     {
         var lines = volume.resolution.x;
@@ -139,7 +130,7 @@ function Isosurfaces( volume, isovalue )
 
         return index;
     }
-    
+
     function interpolated_vertex( v0, v1, s )
     {
 	var i0 = v0.x + (v0.y* volume.resolution.x) + (v0.z*volume.resolution.x * volume.resolution.y);
@@ -149,9 +140,8 @@ function Isosurfaces( volume, isovalue )
 	var s1 = volume.values[i1][0];
 	
 	var t = (s-s0)/(s1-s0);
-	//harf
-        //return new THREE.Vector3().addVectors( v0, v1 ).divideScalar( 2 );
-	return new THREE.Vector3().addVectors(v0.multiplyScalar(1-t),v1.multiplyScalar(t));
+
 	
+	return new THREE.Vector3().addVectors( v0.multiplyScalar(1-t), v1.multiplyScalar(t)); 
     }
 }
