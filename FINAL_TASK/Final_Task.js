a57d964d200ac2ba470760bc2a69404798512891
古田琢人 <furutatakuto@FURUTA-MAC.local>
2017-05-31 12:53:41 +0900

    0531-09

--- FINAL_TASK/Final_Task.js
+++ FINAL_TASK/Final_Task.js
@@ -1,4 +1,4 @@
-function main()
+function main(normal)
 {
     var volume = new KVS.LobsterData();
     var screen = new KVS.THREEScreen();
@@ -15,7 +15,7 @@ function main()
 
    // var isovalue = 128;
     var point = new THREE.Vector3(60,60,17);
-    var normal = new THREE.Vector3(0,0,1);
+    //var normal = new THREE.Vector3(0,0,1);
     var surfaces = SlicePlane( volume, point, normal );
     screen.scene.add( surfaces );
 
